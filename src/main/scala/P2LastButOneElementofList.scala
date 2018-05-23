695e1274f274b16b47d9b6ce062b53b13e1bb920
iamshreeram <shreeram.v@live.com>
2018-05-22 19:31:04 -0600

    2. Penultimate element of the list

--- src/main/scala/P2LastButOneElementofList.scala
+++ src/main/scala/P2LastButOneElementofList.scala
@@ -2,31 +2,14 @@ import P1LastElementofList.last
 
 object P2LastButOneElementofList {
 
-  /*
-  def lastButOne[A](l:List[A]):A = {
-    case head => head
-    case _, LastButOneElement :: lastElement => LastButOneElement
-    case _ => throw new NoSuchElementException
-  }
-*/
-
-  /*
-  def last[A](l:List[A]):A = l match {
-    case h :: Nil => h
-    case _ :: tail => last(tail)
-    case _ => throw new NoSuchElementException
-  }
-  */
-
-  def last[A](l:List[A]):A = l match {
-    case h :: Nil => h
-    case List(_, tail) => tail
-    case _ :: tail => last(tail)
+  def penultimate[A](l:List[A]):A = l match {
+    case h :: List(t) => h
+    case _ :: tail => penultimate(tail)
     case _ => throw new NoSuchElementException
   }
 
   def main(args: Array[String]): Unit = {
     //println(lastButOne(List(1,2,3,4,5)))
-    println(last(List(1,2,3,4,5)))
+    println(penultimate(List(1, 1, 2, 3, 5, 8)))
   }
 }
