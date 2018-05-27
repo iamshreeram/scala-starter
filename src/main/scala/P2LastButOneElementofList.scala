4a134fb8be768ef45ea51bdd8442548d97526b14
iamshreeram <shreeram.v@live.com>
2018-05-27 07:11:54 -0600

    2. Penultimate element of the list

--- src/main/scala/P2LastButOneElementofList.scala
+++ src/main/scala/P2LastButOneElementofList.scala
@@ -1,16 +1,24 @@
 import P1LastElementofList.last
 
 object P2LastButOneElementofList {
-
+  
   /* Procedural :
-    A smart way to solve the problem is to consider that the penultimate element of a list of n elements is the
-    last element of the first (n - 1) elements. Scala provides a method called "init()" which
-    returns all elements except the last.
-   */
-  // init() --> Default method
+  A smart way to solve the problem is to consider that the penultimate element of a list of n elements is the
+  last element of the first (n - 1) elements. Scala provides a method called "init()" which
+  returns all elements except the last.
+ */
+
+  // init() --> Returns all elements except the last
   def defaultPenultimate[A](l:List[A]):A = {
     if (l.isEmpty) throw new NoSuchElementException
     l.init.last //"init()" which returns all elements except the last
+  }
+
+  // takeRight() --> Selects the last n elements from the list.
+  def defaultlastNth[A](n: Int, l: List[A]): A = {
+    if (n <= 0) throw new IllegalArgumentException
+    if (n > l.length) throw new NoSuchElementException
+    l.takeRight(n).head
   }
 
   def penultimate[A](l:List[A]):A = l match {
